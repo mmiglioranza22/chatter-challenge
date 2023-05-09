2f10db72d435c7f83b6868dde8273f849e8d7d38
Manuel Miglioranza <mmiglioranza22@gmail.com>
2023-05-08 22:49:05 -0300

    minor utils changes

--- chatter-frontend-main/src/components/Loading.tsx
+++ chatter-frontend-main/src/components/Loading.tsx
@@ -1,4 +1,4 @@
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
 
 export const LoadStart = () => {Loading.hourglass('Cargando...');}
-export const LoadRemove = (ms: number) => {Loading.remove(ms)};
+export const LoadRemove = (ms?: number ) => {Loading.remove(ms)};
--- chatter-frontend-main/src/utils/utils.ts
+++ chatter-frontend-main/src/utils/utils.ts
@@ -3,14 +3,12 @@ export const validateLogin = (values: LoginData) => {
   const error: Record<any, string> = {}
   const { email, password } = values
   const validEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
-  
   if (!email.length) {
     error.email = 'Ingrese su email'
   }
-
   if (email.length && !validEmail.test(email)) {
     error.email = 'Email inválido'
-  }
+  } 
   if (!password.length) {
     error.password = 'Ingrese su contraseña'
   }
