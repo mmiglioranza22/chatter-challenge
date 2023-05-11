ba4072b52c2e8b4d72187ce5af8a06f19cf8134f
Manuel Miglioranza <mmiglioranza22@gmail.com>
2023-05-11 15:56:31 -0300

    TIcket component styles almost done WIP

--- chatter-frontend-main/src/components/Ticket/Ticket.tsx
+++ chatter-frontend-main/src/components/Ticket/Ticket.tsx
@@ -1,5 +1,5 @@
 import { Modal } from 'react-bootstrap';
-import { TicketData } from '../../types/chat';
+import { TicketData, TicketStatus } from '../../types/chat';
 import styled from 'styled-components';
 
 
@@ -23,26 +23,87 @@ const handleClickOutside = () => {
   }
 }
 
-const LeftSide = 
+const Container = styled.div`
+  position: relative;
+  background: ${status === TicketStatus.CLOSED ? '#01db77' : '#ff3633'};
+  display: flex;
+  justify-content: space-evenly;
+  color: #FFF;
+  min-width: 335px;
+  min-height: 130px;
+`
+
+const LeftSide = styled.div`
+  width: 335px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-around;
+  padding: 16px
+` 
+
+const RightSide = styled.div`
+  width: 92px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: space-around;
+  padding: 16px
+`
+
+const Description = styled.div`
+  max-width: 256px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`
+
+const Title = styled.div`
+  font-weight: 700;
+`
+const Brand = styled.div`
+  background: #FFF;
+  border: 1px solid transparent;
+  border-radius: 6px;
+  color: ${status === TicketStatus.OPEN ? '#63656A' : '#ff3633'};
+  padding: 0px 5px;
+`
+
+const Tag = styled.div`
+  border: 1px solid #FFF;
+  border-bottom-color: transparent;
+  border-top-color: transparent;
+  border-right-color: transparent;
+  padding: 0px 12px;
+  margin-left: 16px;
+`
+const SubContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`
 
   return (
     <Modal className="text-chatter-black" show={isOpen} centered onHide={handleClickOutside}>
-      <Modal.Header className="justify-content-center" closeButton>
-        <Modal.Title>Title</Modal.Title>
-      </Modal.Header>
+      <Modal.Header className="justify-content-center" closeButton />
       <Modal.Body className="justify-content-center text-center">
-      <div>{id}</div>
-      <div>{title}</div>
-      <div>{description}</div>
-      <div>{tag}</div>
-      <div>{brand}</div>
-      <div>{date?.toString()}</div>
-      <div>{priority}</div>
-      <div>{status}</div>
+        <Container>
+          <LeftSide>
+            <Title>{title}</Title>
+            <Description>{description}</Description>
+            <SubContainer>
+              <Brand>{brand?.toUpperCase()}</Brand><Tag>{tag?.toUpperCase()}</Tag>
+            </SubContainer>
+          </LeftSide>
+          {/* separator? */}
+          <RightSide>
+            <div>{date?.toLocaleDateString()}</div>
+            <Brand>{priority === 0 ? 'ALTA' : 'BAJA'}</Brand>
+            <div>#{id}</div>
+          </RightSide>
+        </Container>
       </Modal.Body>
-      <Modal.Footer className="justify-content-center">
-        Footer
-      </Modal.Footer>
     </Modal>
 
   )
