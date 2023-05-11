e7a221ec681e1229d777101b02aaf7df2916c7c0
Manuel Miglioranza <mmiglioranza22@gmail.com>
2023-05-11 17:29:56 -0300

    svg used for ticket inner borders

--- chatter-frontend-main/src/components/Ticket/Ticket.tsx
+++ chatter-frontend-main/src/components/Ticket/Ticket.tsx
@@ -27,28 +27,31 @@ const Container = styled.div`
   position: relative;
   background: ${status === TicketStatus.CLOSED ? '#01db77' : '#ff3633'};
   display: flex;
-  justify-content: space-evenly;
   color: #FFF;
   min-width: 335px;
   min-height: 130px;
 `
 
 const LeftSide = styled.div`
-  width: 335px;
+  min-width: 335px;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   justify-content: space-around;
-  padding: 16px
+  padding: 16px;
+  border-right-style: dashed;
+  border-right-width: 2px; 
+  flex-grow: 1;
 ` 
 
 const RightSide = styled.div`
-  width: 92px;
+  min-width: 92px;
   display: flex;
   flex-direction: column;
   align-items: flex-end;
   justify-content: space-around;
-  padding: 16px
+  padding: 16px;
+  flex-grow: 1;
 `
 
 const Description = styled.div`
@@ -65,7 +68,7 @@ const Brand = styled.div`
   background: #FFF;
   border: 1px solid transparent;
   border-radius: 6px;
-  color: ${status === TicketStatus.OPEN ? '#63656A' : '#ff3633'};
+  color: ${status === TicketStatus.OPEN ? '#63656A' : '#01db77'};
   padding: 0px 5px;
 `
 
@@ -84,12 +87,22 @@ const SubContainer = styled.div`
   align-items: center;
 `
 
+const Svg = styled.svg`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  transform: translate(85px, 0) !important;
+`
+
   return (
     <Modal className="text-chatter-black" show={isOpen} centered onHide={handleClickOutside}>
       <Modal.Header className="justify-content-center" closeButton />
       <Modal.Body className="justify-content-center text-center">
         <Container>
           <LeftSide>
+            <Svg width="32" height="84" viewBox="0 0 32 84" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M16 16C7.16345 16 0 8.83656 0 0H32C32 8.83656 24.8365 16 16 16ZM32 84C32 75.1635 24.8365 68 16 68C7.16345 68 0 75.1635 0 84H32Z" fill="white"/>
+            </Svg>
             <Title>{title}</Title>
             <Description>{description}</Description>
             <SubContainer>
